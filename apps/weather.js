ec7a662ee5bffc1cbb8c99263c7b6c968caff236
pintux <pintusa@tiscali.it>
2015-07-20 14:23:23 +0200

    improved weather

--- apps/weather.js
+++ apps/weather.js
@@ -6,11 +6,11 @@ function run(city, chat, cb){
   var city = city || 'Cagliari';
   var reqURL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q='+city;
   http.get(reqURL, function(err, response, body){
-    var weatherStat = JSON.parse(body);
+
     if(!err && response.statusCode === 200){
-          var w = JSON.parse(body);
+          var weatherStat = JSON.parse(body);
           var message = 'Current Weather in ' +
-                        city.toUpperCase() + ':\n\n' +
+                        weatherStat.name.toUpperCase() + ':\n\n' +
                         weatherStat.weather[0].description + '\n' +
                         'Temperature: ' +
                         weatherStat.main.temp + ' Celsius\n' +
@@ -19,7 +19,7 @@ function run(city, chat, cb){
                         'Humidity: ' +
                         weatherStat.main.humidity + '%\n\n' +
                         'Yo!';
-            debug('Weather:  ' + w);
+            debug('Weather:  ' + weatherStat);
             cb(null, {'text':message}); //supported types are: text
 
     } else {
